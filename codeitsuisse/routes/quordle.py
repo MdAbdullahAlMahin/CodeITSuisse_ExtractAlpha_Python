ae19f8b07553069d85c9230a3b59ce98a398818e
Sabrina <sabsabrinacheung@gmail.com>
2022-09-17 20:30:20 +0800

    quordle1.2

--- codeitsuisse/routes/quordle.py
+++ codeitsuisse/routes/quordle.py
@@ -5,34 +5,48 @@ import json
 
 from flask import request
 
-from codeitsuisse import app
-
 def answer(test):
     MAX = len(test.get('attempts'))
 
     book = dict()
+    result = dict()
 
-    for i in range(len(test.get('attempts'))):
+    for i in test.get('answers'):
         
-        for j in test.get('attempts')[i]:
-            if book.get(j) is None:
-                book[j] = i
-    book = OrderedDict(sorted(book.items()))
-    print(book)
+        for j in range(len(i)):
+            if book.get(i[j]) is None:
+                book[i[j]] = [j]
+            else:
+                book[i[j]].append(j)
+    
+
+    for i,j in enumerate(test.get('attempts')):
+        
+        for k in range(len(j)):
+            check = book.get(j[k])
+            if check is not None and k in check:
+                book[j[k]].remove(k)
+            
+                if book[j[k]] == [] and result.get(j[k]) is None:
+                    result[j[k]] = i
+                    
+            elif result.get(j[k]) is None:
+                result[j[k]] = i
+
+    result = OrderedDict(sorted(result.items()))
     ans = ''
 
-    for i in range(65, 91):
-        if book.get(chr(i)) is None:
+    for i in range(65,91):
+        temp = result.get(chr(i))
+        
+        if temp is not None:
+            ans += str(MAX-temp) if temp>0 else ''
+        else:
             ans += str(MAX)
-        elif temp:= book.get(chr(i)):
-            ans += str(MAX-temp)
-    
     return ans
-
 
 logger = logging.getLogger(__name__)
 @app.route('/quordleKeyboard', methods=['POST'])
 def quordle():
     data = request.get_json()
-
-    return json.dumps(dict(part1=answer(data)))
+    return json.dumps(dict(part1=answer(data), part2=''))
