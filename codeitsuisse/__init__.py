6e6d71a15e88ff1da799464398ebc6041930620b
MdAbdullahAlMahin <u3576752@connect.hku.hk>
2022-09-17 11:12:36 +0800

    Testing tickerStreamPart1

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.tickerStreamPart1
-import codeitsuisse.routes.tickerStreamPart2
+# import codeitsuisse.routes.tickerStreamPart2
 
 
 
